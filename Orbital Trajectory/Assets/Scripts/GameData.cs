20c5f54bfdcf6b21d2dcf7223921077f05b19bcc
Zachary Shaver <54924096+PAhurrican@users.noreply.github.com>
2024-02-16 14:40:16 -0500

    Currently making Level SelectFunctionality

--- Orbital Trajectory/Assets/Scripts/GameData.cs
+++ Orbital Trajectory/Assets/Scripts/GameData.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameData
 {
+    static Scene LevelSelect;
     string[] levelArray;
+    public string[] getAllLevels()
+    {
+        Scene scenes = SceneManager.GetSceneByPath("Assets/Scenes/Completed_Levels");
+
+        string[] a = { "a"};
+        return a;
+    }
+    public static void LoadLevelSelect()
+    {
+
+    }
 }
 
--- Orbital Trajectory/Assets/Scripts/LoadingSceneTrigger.cs
+++ Orbital Trajectory/Assets/Scripts/LoadingSceneTrigger.cs
@@ -24,4 +24,9 @@ public class LoadingSceneTrigger : MonoBehaviour
         }
         
     }
+    public void LoadLevelSelect()
+    {
+        GameData.LoadLevelSelect();
+
+    }
 }
