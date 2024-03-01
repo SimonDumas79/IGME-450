580d8ceba2588717e9588f37f0c4b8d5173bcc93
Simon Dumas <sad1667@rit.edu>
2024-03-01 10:50:34 -0500

    moving planets added

--- Orbital Trajectory/Assets/Scripts/MovingPlanet.cs
+++ Orbital Trajectory/Assets/Scripts/MovingPlanet.cs
@@ -7,23 +7,37 @@ public class MovingPlanet : MonoBehaviour
     //the object being orbited around
     public Transform centralBody;
 
-    public Vector3 startDirection;
+    //Direction to offset the orbiting body by the radius
+    public Vector2 startDirection;
 
     //amount of time taken to complete one full orbit
     public float baseTimeForCycle;
-    private float timeForCycle;
 
+    //rotation in radians around the central body scaled by baseTimeForCycle
+    private float rotation;
+
+    //offset from central body to orbit
     public float radius;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        transform.position = centralBody.position + startDirection * radius;
+        startDirection = startDirection.normalized;
+        
+        rotation = Mathf.Deg2Rad * Vector3.Angle(Vector3.right, startDirection);
+
+        if(startDirection.y < 0)
+        {
+            rotation = Mathf.PI + (Mathf.PI - rotation);
+        }
+        Debug.Log(rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Mathf.Sin(0), Mathf.Sin(0), 0);
+        rotation += 2 * Mathf.PI * (Time.deltaTime / baseTimeForCycle);
+
+        transform.position = new Vector3(radius * Mathf.Cos(rotation), radius * Mathf.Sin(rotation), 0);
     }
 }
